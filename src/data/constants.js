7548bb516086735339819841c66e709d59ce3c9a
BhargavKumar9390 <158072373+BhargavKumar9390@users.noreply.github.com>
2024-10-29 16:56:40 -0400

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/BhargavKumar9390",
   resume:
-    "https://drive.google.com/file/d/13LS9afB-WUzjiUaoJFxxBFocFJ2BG0TJ/view?usp=drive_link",
+    "https://drive.google.com/file/d/1hZtWWXmSnFNTLbuk5L36YijFzMLLdUVp/view?usp=sharing",
   linkedin: "http://www.linkedin.com/in/bhargavkumarkalvakuntla",
   twitter: "https://twitter.com/IconBharga3636",
   insta:"https://www.instagram.com/bhargav_icon?igsh=aGp5NHVrdG1uYjI1&utm_source=qr",
